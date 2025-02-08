b73dd0444814768434ea36e4496d2017aae82099
Szymon <skasprzyk52@gmail.com>
2025-02-08 10:12:04 +0100

    Polaczenie z baza

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { supabase } from "./config";
 
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -11,12 +11,32 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Sample Route
-app.get("/", (req, res) => {
-  res.send("TripMate AI Backend is running 🚀");
+
+app.get("/profiles", async (req, res) => {
+  try {
+      const { data, error } = await supabase
+          .from("profiles") 
+          .select("*"); 
+
+      if (error) {
+          throw error;
+      }
+      console.log(data);
+      res.send(data);
+  } catch (error) {
+      console.error("Data fetch error:", error);
+      res.status(500).json({ error: "Data fetch error." });
+  }
 });
 
-// Start Server
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+// Sample Route
+app.get("/", (req, res) => {
+  res.send("TripMate AI Backend is running ");
+
 });
+
+app.listen(PORT, () => {
+  console.log(`🚀 Serwer działa na http://localhost:${PORT}`);
+});
+
+

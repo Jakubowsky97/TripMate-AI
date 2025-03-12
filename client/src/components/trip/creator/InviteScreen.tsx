d0a38e2df34ce585c7d06c89d0ae98fbaceec76d
Jakub <jakubowskyy97@gmail.com>
2025-03-12 08:24:06 +0100

    Enhance InviteScreen: add hover effect and cursor pointer to copy icon for better user interaction

--- client/src/components/trip/creator/InviteScreen.tsx
+++ client/src/components/trip/creator/InviteScreen.tsx
@@ -25,7 +25,11 @@ const InviteFriends = ({ nextStep, prevStep }: InviteScreenProps) => {
                 <label className="block text-base text-[#142F32] font-bold mb-1">Trip code</label>
                 <div className="flex items-center flex-row bg-gray-100 px-4 py-2 rounded-lg gap-4">
                     <p className="text-gray-800 font-mono">{tripCode}</p>
-                    <FaRegCopy onClick={copyToClipboard}/>
+                    <FaRegCopy 
+                    onClick={copyToClipboard}
+                    className="text-gray-500 hover:text-blue-500 cursor-pointer"
+                    />
+
                 </div>
             </div>
             <div>
